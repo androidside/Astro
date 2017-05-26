69bbe9207f1933e7b2bd08e1fd271a5d80e0722e
U-GS66-WHITE\labuser <GS66-WHITE+labuser@GS66-WHITE.ndc.nasa.gov>
2017-05-26 00:25:49 -0500

    Palestine Texas

--- RaDecToAzElJava/src/src/main.java
+++ RaDecToAzElJava/src/src/main.java
@@ -1,15 +1,34 @@
 package src;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 
 public class main {
-
+	
+	public static double raDegrees = 279.38345;
+	public static double decDegrees = 38.8035;
+	
+	public static double latitude = 31.779933; 
+	public static double longitude = -95.717039;
+		
 
 
 	public static void main( String[] args ) {
+		
+		Calendar now = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+
 		//Just for Testing
-		Location l = new Location(34.4717,104.2455,-6); //Fort Sumner
-		DateTime dt = new DateTime(2016, 9, 21, 3, 14, 00);
-		SkyPos s = new SkyPos(40.75, 11.14);
+		Location l = new Location(latitude, longitude, 0); //0 = UTC timezone	
+		int hours = now.get(Calendar.HOUR_OF_DAY);
+		int minutes = now.get(Calendar.MINUTE);
+		int seconds = now.get(Calendar.SECOND);
+		int year = now.get(Calendar.YEAR);
+		int month = now.get(Calendar.MONTH) + 1;
+		int day = now.get(Calendar.DAY_OF_MONTH);
+		
+		DateTime dt = new DateTime(year, month, day, hours, minutes, seconds);
+		SkyPos s = new SkyPos(raDegrees/15, decDegrees);
 		
 		Astro a =new Astro();
 		
@@ -17,3 +36,4 @@ public class main {
 	}
 
 }
+
